d544026a005f5778619da68e2ca760205443c9a2
vigneshb03 <vigneshvickyreddy030@gmail.com>
2022-09-18 11:16:43 +0530

    dashboard screen header done

--- angular_basics/src/app/components/dashboard/dashboard-routing.module.ts
+++ angular_basics/src/app/components/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CareerComponent } from './career/career.component';
 import { DashboardComponent } from './dashboard.component';
+import { SupportComponent } from './support/support.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'career', component: CareerComponent },
+  { path: 'support', component: SupportComponent }
 ];
 
 @NgModule({
--- angular_basics/src/app/components/dashboard/dashboard.module.ts
+++ angular_basics/src/app/components/dashboard/dashboard.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { AboutComponent } from './about/about.component';
+import { CareerComponent } from './career/career.component';
+import { SupportComponent } from './support/support.component';
+import { HeaderComponent } from '../shared/header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AboutComponent,
+    CareerComponent,
+    SupportComponent, HeaderComponent
+  ], bootstrap: [HeaderComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
-  ]
+  ], schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class DashboardModule { }
